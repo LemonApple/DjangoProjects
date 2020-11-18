8fece2ef1959e4d1ff2a212b774171a276ec7766
陽明 <16509526+LemonApple@users.noreply.github.com>
2020-11-18 19:37:03 +0800

    增加了一些页面

--- login/article/models.py
+++ login/article/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from loginpage.models import Users
+
+
+class Types(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(default="暂无内容")
+    type = models.ForeignKey(Types, on_delete=models.CASCADE)  # on_delete=models.CASCADE为实现主外键一致，在Types删除时，文章一并删除
+    author = models.ForeignKey(Users, on_delete=models.CASCADE)
--- login/article/views.py
+++ login/article/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render, redirect
 
 
 # Create your views here.
+from article.models import Types
+
+
 def articlelist(request):
     if request.session.get('id'):
         return render(request, 'article-list.html')
     else:
         return redirect('/login/login/')
+
+
+def articleadd(request):
+    return render(request, 'article-add.html')
+
+
+def typeinit(request):
+    type = Types(title='大陆新闻')
+    type.save()
+    type = Types(title='港澳台新聞')
+    type.save()
+    type = Types(title='US News')
+    type.save()
+    return render(request, 'temp.html', {"info":"文章类型初始化完成"})
