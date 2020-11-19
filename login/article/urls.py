f8444f6de5037c4c0eee0f88b872d4f5adc259f4
陽明 <16509526+LemonApple@users.noreply.github.com>
2020-11-19 21:32:40 +0800

    懒得写了

--- login/article/urls.py
+++ login/article/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from article import views
 
 urlpatterns = [
+    path('articledel/', views.articledel),
     path('typeinit/',views.typeinit),
     path('articleadd/',views.articleadd),
     path('articlelist/',views.articlelist),
--- login/article/views.py
+++ login/article/views.py
@@ -1,20 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
-from article.models import Types
+from article.models import Types, Articles
 
 
 def articlelist(request):
-    if request.session.get('id'):
-        return render(request, 'article-list.html')
-    else:
-        return redirect('/login/login/')
+    articles=Articles.objects.all()
+    return render(request, 'article-list.html', {'articles':articles})
 
 
 def articleadd(request):
-    return render(request, 'article-add.html')
-
+    # 遍历之后types拿到的是一个列表
+    if request.method == 'GET':
+        types=Types.objects.all()
+        # {'types':types}是添加到模板上下文的一个字典
+        return render(request, 'article-add.html', {'types':types})
+    else:
+        title = request.POST.get("title")
+        type = request.POST.get("type")
+        content = request.POST.get("content")
+        author = request.session.get("id")
+        # 把拿到的数据写入数据库
+        article = Articles(title=title, content=content, type_id=type, author_id=author)
+        article.save()
+        return HttpResponse("文章添加成功！")
 
 def typeinit(request):
     type = Types(title='大陆新闻')
@@ -24,3 +35,12 @@ def typeinit(request):
     type = Types(title='US News')
     type.save()
     return render(request, 'temp.html', {"info":"文章类型初始化完成"})
+
+
+def articledel(request):
+    # 从ajax接收data中的id
+    id=request.POST.get('id')
+    # 找到对应id，通过filter会返回一个列表，第0条就是我们想要的元素，然后进行删除
+    Articles.objects.filter(id=id)[0].delete()
+    # 必须返回信息给ajax，否则会一直等待
+    return HttpResponse("1")
